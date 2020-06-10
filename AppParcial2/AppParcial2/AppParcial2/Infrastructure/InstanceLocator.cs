910306bcdbd2bb643fbf29b672fb1841c27b3aa3
Jorge Eduardo Robles Maldonado <jorgeduardorobles@hotmail.com>
2020-06-10 01:38:16 -0400

    Paso a medias

---
+++ AppParcial2/AppParcial2/AppParcial2/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace AppParcial2.Infrastructure
+{
+    using AppParcial2.ViewModels;
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
---
+++ AppParcial2/AppParcial2/AppParcial2/ViewModels/CountryViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppParcial2.ViewModels
+{
+    class CountryViewModel:BaseViewModel
+    {
+        //apiservice
+
+        #region Attributes
+        private string name;
+        private string alpha2code;
+        private string capital;
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { SetValue(ref this.name, value); }
+        }
+        public string Capital
+        {
+            get { return this.capital; }
+            set { SetValue(ref this.capital, value); }
+        }
+        public string Alpha2code
+        {
+            get { return this.alpha2code; }
+            set { SetValue(ref this.alpha2code, value); }
+        }
+        #endregion
+
+        #region Constructor
+        public CountryViewModel()
+        {
+            this.LoadCountry();
+        }
+        #endregion
+
+        #region Methods
+        private void LoadCountry()
+        {
+            //llamar api
+        }
+        #endregion
+    }
+}
